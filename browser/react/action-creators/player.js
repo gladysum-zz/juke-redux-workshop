b37c3f9f21fd7b8d5d8c5e724e177ee1738a6359
Gladys Um <gladysum@gmail.com>
2016-12-06 18:28:52 -0500

    part done

--- browser/react/action-creators/player.js
+++ browser/react/action-creators/player.js
@@ -1,34 +1,67 @@
 import {
-	START_PLAYING,
-	STOP_PLAYING,
-	SET_CURRENT_SONG,
-	SET_LIST
+    START_PLAYING,
+    STOP_PLAYING,
+    SET_CURRENT_SONG,
+    SET_LIST
 } from '../constants';
 
-export const startPlaying = function() {
-	return {
-		type: START_PLAYING
-	}	
-}
+const startPlaying = () => ({ type: START_PLAYING });
 
-export const stopPlaying = function() {
-	return {
-		type: STOP_PLAYING
-	}	
-}
+const stopPlaying = () => ({ type: STOP_PLAYING });
 
-export const setCurrentSong = function(song) {
-	return {
-		type: SET_CURRENT_SONG,
-		currentSong: song
-	}	
-}
+const setCurrentSong = (currentSong) => ({ 
+  type: SET_CURRENT_SONG,
+  currentSong
+});
 
-export const setCurrentSongList = function(songList) {
-	return {
-		type: SET_LIST,
-		currentSongList: songList
-	}	
-}
+const setCurrentSongList = (currentSongList) => ({ 
+  type: SET_LIST,
+  currentSongList
+});
 
+export const play = () => dispatch => {
+  AUDIO.play();
+  dispatch(startPlaying());
+};
 
+export const pause = () => dispatch => {
+  AUDIO.pause();
+  dispatch(stopPlaying());
+};
+
+export const load = (currentSong, currentSongList) => dispatch => {
+  AUDIO.src = currentSong.audioUrl;
+  AUDIO.load();
+  dispatch(setCurrentSongList(currentSongList));
+  dispatch(setCurrentSong(currentSong));
+};
+
+export const startSong = (song, list) => dispatch => {
+  dispatch(pause());
+  dispatch(load(song, list));
+  dispatch(play());
+};
+
+export const toggle = () => (dispatch, getState) => {
+  const { isPlaying } = getState();
+  if (isPlaying) dispatch(pause()); 
+  else dispatch(play());
+};
+
+export const toggleOne = (selectedSong, selectedSongList) => 
+  (dispatch, getState) => {
+    const { currentSong } = getState();
+    if (selectedSong.id !== currentSong.id)
+      dispatch(startSong(selectedSong, selectedSongList));
+    else dispatch(toggle());
+};
+
+export const next = () => 
+  (dispatch, getState) => {
+    dispatch(startSong(...skip(1, getState()));
+};
+
+export const prev = () => 
+  (dispatch, getState) => {
+    dispatch(startSong(...skip(-1, getState()));
+};
--- browser/react/reducers/player-reducer.js
+++ browser/react/reducers/player-reducer.js
@@ -1,6 +1,8 @@
 import {
-  START_PLAYING,
-  STOP_PLAYING
+    START_PLAYING,
+    STOP_PLAYING,
+    SET_CURRENT_SONG,
+    SET_LIST
 } from '../constants';
 
 export const initialPlayerState = {
@@ -16,6 +18,14 @@ export default function (state = initialPlayerState, action) {
 
   switch (action.type) {
 
+    case SET_CURRENT_SONG:
+      newState.currentSong = action.currentSong;
+      break;
+
+    case SET_LIST:
+      newState.currentSongList = action.currentSongList;
+      break;  
+    
     case START_PLAYING:
       newState.isPlaying = true;
       break;
